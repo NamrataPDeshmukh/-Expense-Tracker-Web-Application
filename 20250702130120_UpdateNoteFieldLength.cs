020bdc3b6f2080c29d7bb00a360dddf0974d9e6b
NamrataPDeshmukh <161303191+NamrataPDeshmukh@users.noreply.github.com>
2025-07-06 16:44:50 +0530

    Add files via upload

---
+++ 20250702130120_UpdateNoteFieldLength.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Expense_Tracker_Application.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateNoteFieldLength : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Note",
+                table: "Transactions",
+                type: "nvarchar(75)",
+                maxLength: 75,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(5)",
+                oldMaxLength: 5,
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Note",
+                table: "Transactions",
+                type: "nvarchar(5)",
+                maxLength: 5,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(75)",
+                oldMaxLength: 75,
+                oldNullable: true);
+        }
+    }
+}
